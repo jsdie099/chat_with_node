935afbc141ace8824bf64b2a37df30b59cee83e6
jsdie099 <julianopaulo.santos@hotmail.com>
2020-08-15 19:21:47 -0300

    instalando as dependências do mongodb e fazendo a primeira conexão e inserção no mesmo

--- src/server.ts
+++ src/server.ts
@@ -6,101 +6,116 @@
 
 import { Socket } from "socket.io";
 
+
 var app = require('../app');
 var debug = require('debug')('chat-with-node-and-socket:server');
 var http = require('http');
 
-/**
- * Get port from environment and store in Express.
+
+/* GET CONNECTION WITH MONGODB */
+
+require('dotenv-safe').config();
+const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect(process.env.MONGO_CONNECTION, function(err: any, conn: { db: (arg0: string | undefined) => any; }){
+  if(err) { return console.log(err); }
+  console.log("conectou no banco de dados!");
+  const globalAny:any = global;
+  globalAny.db = conn.db(process.env.MONGO_DB);
+  //coloque todo o código antigo do www aqui dentro
+  /**
+   * Get port from environment and store in Express.
  */
+  var port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+  /**
+   * Create HTTP server.
+   */
 
-/**
- * Create HTTP server.
- */
+  var server = http.createServer(app);
 
-var server = http.createServer(app);
+  var io = require('socket.io')(server);
+  io.on('connection', function(socket: Socket){
+    console.log('a user connected');
+    socket.on('chat message', function(msg:string){
+      io.emit('chat message', msg);
+      console.log('message: ' + msg);
+    });
 
-var io = require('socket.io')(server);
-io.on('connection', function(socket: Socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg:string){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+
+    socket.on('disconnect', ()=>{
+      console.log('user Disconnected');
+    })
   });
 
+  /**
+   * Listen on provided port, on all network interfaces.
+   */
 
-  socket.on('disconnect', ()=>{
-    console.log('user Disconnected');
-  })
-});
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+  /**
+   * Normalize a port into a number, string, or false.
+   */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+  function normalizePort(val: string) {
+    var port = parseInt(val, 10);
 
-/**
- * Normalize a port into a number, string, or false.
- */
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
 
-function normalizePort(val: string) {
-  var port = parseInt(val, 10);
+    if (port >= 0) {
+      // port number
+      return port;
+    }
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
+    return false;
   }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+  /**
+   * Event listener for HTTP server "error" event.
+   */
 
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error: { syscall: string; code: string; }) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
+  function onError(error: { syscall: string; code: string; }) {
+    if (error.syscall !== 'listen') {
       throw error;
+    }
+
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
   }
-}
 
-/**
- * Event listener for HTTP server "listening" event.
- */
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
 
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  }
+
+})
+
+
