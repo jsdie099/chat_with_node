16b4a22e9353582c8a58609061401a9da9949f0f
jsdie099 <julianopaulo.santos@hotmail.com>
2020-08-15 16:26:46 -0300

    deixando o express montar toda a estrutura de pastas, e somente passando tudo para typescript

---
+++ routes/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/chat", (req, res)=>{
+  res.render('chat',{})
+})
+
+module.exports = router;
